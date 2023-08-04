88ad47708ce541622ef4de93f4b4a5eefd0c9c95
tejasgadge <tejasgadge2882@gmail.com>
2023-08-04 08:32:18 +0530

    Authentication and Display functionality added

---
+++ client/src/pages/IndexPage.jsx
@@ -0,0 +1,31 @@
+import {useEffect, useState} from "react";
+import axios from "axios";
+import {Link} from "react-router-dom";
+import Image from "../Image.jsx";
+
+export default function IndexPage() {
+  const [places,setPlaces] = useState([]);
+  useEffect(() => {
+    axios.get('/places').then(response => {
+      setPlaces(response.data);
+    });
+  }, []);
+  return (
+    <div className="mt-12 grid gap-x-12 gap-y-8 grid-cols-2 md:grid-cols-3 lg:grid-cols-5 px-8">
+      {places.length > 0 && places.map(place => (
+        <Link to={'/place/'+place._id}>
+          <div className="bg-gray-500 mb-2 rounded-2xl flex">
+            {place.photos?.[0] && (
+              <Image className="rounded-2xl object-cover aspect-square " src={place.photos?.[0]} alt=""/>
+            )}
+          </div>
+          <h2 className="font-bold truncate">{place.address}</h2>
+          <h3 className="text-sm text-gray-500 truncate">{place.title}</h3>
+          <div className="mt-1">
+            <span className="font-bold">${place.price}</span> per night
+          </div>
+        </Link>
+      ))}
+    </div>
+  );
+}
